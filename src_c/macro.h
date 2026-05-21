d21b6424329c9cf463dcff45819aee1e1a0c1e8f
Mircio-Ferreira <mfsn@cesar.school>
2026-05-21 10:51:38 -0300

    alterando depedencia do macro

--- src_c/macro.h
+++ src_c/macro.h
@@ -3,3 +3,9 @@
 #define BIG 15
 #define LEN_TEST 30
 #define FLAG_ERASER_TEST 1
+
+
+
+#define FILE_NAME_SMALL "case_tests_c/SMALL_TEST"
+#define FILE_NAME_MIDDLE "case_tests_c/MIDDLE_TEST"
+#define FILE_NAME_BIG "case_tests_c/BIG_TEST"
--- src_c/main.c
+++ src_c/main.c
@@ -3,21 +3,17 @@
 #include "macro.h"
 #include "file.h"
 
-#define FILE_NAME_SMALL "case_tests/SMALL_TEST"
-#define FILE_NAME_MIDDLE "case_tests/MIDDLE_TEST"
-#define FILE_NAME_BIG "case_tests/BIG_TEST"
 
 int main(){
 
     char alphabet[30];
     int n = 30;
-
     
+    for (int i = 0; i < n; i++) alphabet[i] = 'A' + i;
+
     eraser_test_file(FILE_NAME_SMALL);
     eraser_test_file(FILE_NAME_MIDDLE);
     eraser_test_file(FILE_NAME_BIG);
-    
-    for (int i = 0; i < n; i++) alphabet[i] = 'A' + i;
 
     for(int i = 0; i < LEN_TEST; i++){
         test(SMALL ,alphabet,FILE_NAME_SMALL);
